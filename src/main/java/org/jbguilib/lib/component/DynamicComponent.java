1650a42292cda0a24f92ac89d0be3f73297a96e6
Joshua Buker <joshbuker04@gmail.com>
2024-06-14 20:08:21 -0700

    Physics Getter/Setters

--- src/main/java/org/jbguilib/lib/component/DynamicComponent.java
+++ src/main/java/org/jbguilib/lib/component/DynamicComponent.java
@@ -4,6 +4,12 @@ public abstract class DynamicComponent extends StaticComponent {
   private float GRAVITY = 10;
   private float mass = 1;
 
+  public float getGRAVITY() { return GRAVITY; }
+  public float getMass() { return mass; }
+
+  public void setGRAVITY(float gRAVITY) { GRAVITY = gRAVITY; }
+  public void setMass(float mass) { this.mass = mass; }
+
   private void applyPhysics() {}
 
   public void run() {
